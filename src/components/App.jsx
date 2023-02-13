e7f7fe1e9b8fcb0ff2e8e71204c5df9a88724660
Skynet4241 <skynet4241@gmail.com>
2023-02-14 01:03:41 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,33 +11,22 @@ import {
 import { Filter } from './Filter/Filter';
 
 export const App = () => {
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const contactsFromLS = JSON.parse(localStorage.getItem('contactlist'));
+    return contactsFromLS.length !== 0
+      ? contactsFromLS
+      : [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ];
+  });
   const [filter, setFilter] = useState('');
-  const [syncLS, setSyncLC] = useState(true);
 
   useEffect(() => {
-    if (syncLS) {
-      const contactsFromLS = JSON.parse(localStorage.getItem('contactlist'));
-      if (contactsFromLS.length !== 0) setContacts(contactsFromLS);
-      setSyncLC(false);
-    } else {
-      localStorage.setItem('contactlist', JSON.stringify(contacts));
-    }
-  }, [contacts, syncLS]);
+    localStorage.setItem('contactlist', JSON.stringify(contacts));
+  }, [contacts]);
 
   const submitHandler = e => {
     e.preventDefault();
